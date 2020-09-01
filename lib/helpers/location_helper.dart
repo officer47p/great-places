26c2e9c3fc71fbd73f71efd5d2460c80ff7d3329
Parsa Hosseini <officer47p@gmail.com>
2020-09-01 10:49:18 +0430

    added the location to address static function to the LocationHelper, now whenever user requests it's location, the app gives her/him an readable address too

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,7 +1,28 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 import '../credentials.dart';
 
 class LocationHelper {
   static String getStaticImageMapUrl({String latitude, String longitude}) {
     return "https://www.mapquestapi.com/staticmap/v5/map?key=$API_KEY&center=$latitude,$longitude&locations=$latitude,$longitude&size=&size=600,170&zoom=16";
+  }
+
+  static Future<String> getPlaceAddress(
+      String latitude, String longitude) async {
+    final url =
+        "http://www.mapquestapi.com/geocoding/v1/reverse?key=$API_KEY&location=$latitude,$longitude";
+    final res = await http.get(url);
+    final aData = json.decode(res.body)["results"][0]["locations"][0];
+    final List addressList = [
+      aData['street'],
+      aData['adminArea6'],
+      aData['adminArea5'],
+      aData['adminArea4'],
+      aData['adminArea3'],
+      aData['adminArea1']
+    ].where((element) => (element as String).length > 0).toList();
+    return addressList.join(" ,");
   }
 }
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -10,6 +10,7 @@ class LocationInput extends StatefulWidget {
 
 class _LocationInputState extends State<LocationInput> {
   String _previewImageUrl;
+  String _address = "fd";
 
   Future<void> getCurrentUserLocation() async {
     Location location = Location();
@@ -35,6 +36,8 @@ class _LocationInputState extends State<LocationInput> {
     }
 
     _locationData = await location.getLocation();
+    final address = await LocationHelper.getPlaceAddress(
+        _locationData.latitude.toString(), _locationData.longitude.toString());
     print(
         "Latitude: ${_locationData.latitude}, Longitude: ${_locationData.longitude}");
     setState(() {
@@ -42,6 +45,7 @@ class _LocationInputState extends State<LocationInput> {
         latitude: _locationData.latitude.toString(),
         longitude: _locationData.longitude.toString(),
       );
+      _address = address;
     });
   }
 
@@ -68,23 +72,52 @@ class _LocationInputState extends State<LocationInput> {
                   fit: BoxFit.cover,
                 ),
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FlatButton.icon(
-              onPressed: getCurrentUserLocation,
-              icon: Icon(Icons.location_on),
-              label: Text("Current Location"),
-              textColor: Theme.of(context).primaryColor,
-            ),
-            FlatButton.icon(
-              onPressed: () {},
-              icon: Icon(Icons.map),
-              label: Text("Select On Map"),
-              textColor: Theme.of(context).primaryColor,
-            )
-          ],
-        )
+        if (_address != null) ...[
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            children: [
+              Text(
+                "Address:",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Text(
+                  _address,
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+        FlatButton.icon(
+          onPressed: getCurrentUserLocation,
+          icon: Icon(Icons.location_on),
+          label: Text("Get Current Location"),
+          textColor: Theme.of(context).primaryColor,
+        ),
+        // Row(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: [
+
+        //     FlatButton.icon(
+        //       onPressed: () {},
+        //       icon: Icon(Icons.map),
+        //       label: Text("Select On Map"),
+        //       textColor: Theme.of(context).primaryColor,
+        //     )
+        //   ],
+        // )
       ],
     );
   }
